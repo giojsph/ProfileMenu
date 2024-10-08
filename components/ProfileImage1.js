8a4ea1c31cc7431146a07b75ebc538b26d4e5033
giojsph <cahiles.giojoseph@gmail.com>
2024-10-08 23:48:28 +0800

    image change to online image

--- components/ProfileImage1.js
+++ components/ProfileImage1.js
@@ -6,10 +6,10 @@ const ProfileImage = () => (
   <View style={styles.profileSection}>
     <Image
       style={styles.profileImage}
-      source={{ uri: 'assets/Profilepic.png' }} 
+      source={{ uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }} 
     />
     <View>
-      <Text style={styles.nameText}>David Robinson</Text>
+      <Text style={styles.nameText}>Gio Joseph</Text>
       <Text style={styles.joinedText}>Joined 1 year ago</Text>
     </View>
   </View>
